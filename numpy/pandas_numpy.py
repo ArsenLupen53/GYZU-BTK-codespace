832ec6f7a686ad24bdc7fbfdeeb9859bb76e69af
Ahmet Berk Güneli <kingnocktis@gmail.com>
2026-05-01 19:15:21 +0300

    numpy gün 2

--- numpy/pandas_numpy.py
+++ numpy/pandas_numpy.py
@@ -9,6 +9,68 @@ Original file is located at
 
 import numpy as np
 
+"""## Dizi oluşturma fonksiyonları"""
+
 print(f"Aralık ile array tanımla: \n {np.arange(0,10,2)}") #ilk eleman dahil, son eleman dahil değil
 
 print(f"Eşit aralıklar ile array: \n {np.linspace(0,9,6)}") #ilk ve son eleman dahil
+
+print(f"full: \n {np.full((2,4),7)}")
+
+print(f"logspace: \n {np.logspace(0,3,4)}") #10^0 ile 10^3 arasında logaritmik aralık 4 sayı oluştur
+
+print(f"birim matris oluşturucu: \n {np.eye(3)}")
+
+print(f"birim matris olutşru: \n {np.identity(4)}")
+
+print(f"empty: \n {np.empty((2,3))}")
+
+arr = np.array([[1,2,3],[4,5,6]])
+empty_like_arr = np.empty_like(arr)
+print(f"empty_like: \n {empty_like_arr}")
+
+print(f"tile: \n {np.tile(([1,2,3]),2)}")
+print(f"repeat: \n {np.repeat(([1,2,3]),2)}")
+
+"""## İndeksleme ve Dilimleme"""
+
+arr = np.array([10,20,30,40,50])
+
+fancy = arr[[0,2,4]]
+print(f"fancy: \n {fancy}")
+
+arr2 = np.array([[10,20],[30,40],[50,60]])
+rows = [0,2]
+cols = [0]
+print(f"seçilen: {arr2[rows,cols]}")
+
+arr = np.array([5,10,15,20,25])
+bool_index = arr[arr > 15]
+print(f"bool_index: {bool_index}")
+
+"""# View vs Copy"""
+
+arr = np.array([10,20,30,40,50])
+arr
+
+slice_view = arr[1:4]
+slice_view[:] = 0
+print(arr)
+
+arr2 = np.array([10,20,30,40,50])
+arr2
+
+slice_copy = arr2[1:4].copy()
+slice_copy[:] = 0
+print(arr2)
+slice_copy
+
+"""#Dizi İşlemleri"""
+
+arr = np.array([1,2,3,4,5])
+print(f"min değerin indeksi {np.argmin(arr)}")
+print(f"max değerin indeksi {np.argmax(arr)}")
+
+arr = np.array([1,2,3,4,5])
+print(f"Kümülatif toplam:  {np.cumsum(arr)}")
+print(f"Kümülatif çarpım: {np.cumprod(arr)}")
